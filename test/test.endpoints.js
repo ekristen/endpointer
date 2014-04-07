4ca0a77d32976d2a89bbb001be49c701019c8441
Erik Kristensen <erik@erikkristensen.com>
2014-04-07 18:40:44 -0400

    adding tests

---
+++ test/test.endpoints.js
@@ -0,0 +1,121 @@
+var domain = require('domain');
+var restify = require('restify');
+
+var manager = require('../lib');
+
+exports.setUp = function(done) {
+  done();
+}
+
+exports.tearDown = function(done) {
+  done();
+}
+
+
+exports.addBasicEndpoint = function(test) {
+  var endpoints = new manager.EndpointManager();
+  var server = restify.createServer();
+
+  endpoints.addEndpoint({
+    name: 'test',
+    description: 'test',
+    method: 'GET',
+    path: '/test',
+    fn: function (req, res, next) {
+      res.send(200);
+    }
+  });
+
+  endpoints.attach(server);
+
+  test.done();
+}
+
+exports.addBasicEndpointWithVersion = function(test) {
+  var endpoints = new manager.EndpointManager();
+  var server = restify.createServer();
+
+  endpoints.addEndpoint({
+    name: 'test',
+    description: 'test',
+    method: 'GET',
+    version: '1.0.0',
+    path: '/test',
+    fn: function (req, res, next) {
+      res.send(200);
+    }
+  });
+
+  test.done();
+}
+
+exports.addMultipleMethodEndpoint = function(test) {
+  var endpoints = new manager.EndpointManager();
+  var server = restify.createServer();
+
+  endpoints.addEndpoint({
+    name: 'test',
+    description: 'test',
+    method: ['GET', 'POST'],
+    path: '/test',
+    fn: function (req, res, next) {
+      res.send(200);
+    }
+  });
+
+  test.done();
+}
+
+exports.addMultipleVersionsEndpoint = function(test) {
+  var endpoints = new manager.EndpointManager();
+  var server = restify.createServer();
+
+  endpoints.addEndpoint({
+    name: 'test',
+    description: 'test',
+    method: 'GET',
+    version: ['1.0.0', '2.0.0'],
+    path: '/test',
+    fn: function (req, res, next) {
+      res.send(200);
+    }
+  });
+
+  test.done();
+}
+
+exports.addMultiplePathsEndpoint = function(test) {
+  var endpoints = new manager.EndpointManager();
+  var server = restify.createServer();
+
+  endpoints.addEndpoint({
+    name: 'test',
+    description: 'test',
+    method: 'GET',
+    version: '1.0.0',
+    path: ['/test', '/test2'],
+    fn: function (req, res, next) {
+      res.send(200);
+    }
+  });
+
+  test.done();
+}
+
+exports.addMultipleVersionsMethodEndpoint = function(test) {
+  var endpoints = new manager.EndpointManager();
+  var server = restify.createServer();
+
+  endpoints.addEndpoint({
+    name: 'test',
+    description: 'test',
+    method: ['GET', 'POST'],
+    version: ['1.0.0', '2.0.0'],
+    path: ['/test', '/test2'],
+    fn: function (req, res, next) {
+      res.send(200);
+    }
+  });
+
+  test.done();
+}
---
+++ test/test.errors.js
@@ -0,0 +1,103 @@
+var domain = require('domain');
+var restify = require('restify');
+
+var manager = require('../lib');
+
+exports.setUp = function(done) {
+  done();
+}
+
+exports.tearDown = function(done) {
+  done();
+}
+
+exports.errorNoPathEndpoint = function(test) {
+  var endpoints = new manager.EndpointManager();
+  var server = restify.createServer();
+
+  try {
+    endpoints.addEndpoint({
+      name: 'test',
+      description: 'test',
+      method: 'GET',
+      fn: function (req, res, next) {
+        res.send(200);
+      }
+    });
+
+    endpoints.attach(server);
+  } catch (e) {
+    test.equal(e.message, "Path is required in an endpoint definition");
+  }
+
+  test.done();
+}
+
+
+exports.errorNoNameEndpoint = function(test) {
+  var endpoints = new manager.EndpointManager();
+  var server = restify.createServer();
+
+  try {
+    endpoints.addEndpoint({
+      description: 'test',
+      method: 'GET',
+      path: '/test',
+      fn: function (req, res, next) {
+        res.send(200);
+      }
+    });
+
+    endpoints.attach(server);
+  } catch (e) {
+    test.equal(e.message, "Name is required in an endpoint definition");
+  }
+
+  test.done();
+}
+
+
+exports.errorNoMethodEndpoint = function(test) {
+  var endpoints = new manager.EndpointManager();
+  var server = restify.createServer();
+
+  try {
+    endpoints.addEndpoint({
+      name: 'test',
+      description: 'test',
+      path: '/test',
+      fn: function (req, res, next) {
+        res.send(200);
+      }
+    });
+
+    endpoints.attach(server);
+  } catch (e) {
+    test.equal(e.message, "Method is required in an endpoint definition");
+  }
+
+  test.done();
+}
+
+
+exports.errorNoFunctionEndpoint = function(test) {
+  var endpoints = new manager.EndpointManager();
+  var server = restify.createServer();
+
+  try {
+    endpoints.addEndpoint({
+      name: 'test',
+      description: 'test',
+      path: '/test',
+      method: 'GET'
+    });
+
+    endpoints.attach(server);
+  } catch (e) {
+    test.equal(e.message, "fn is required to define an endpoint");
+  }
+
+  test.done();
+}
+
+
