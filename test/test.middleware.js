65fe551a0de1cdd487af623b2543ce26955f362e
Erik Kristensen <erik@erikkristensen.com>
2014-05-05 16:34:00 -0400

    adding tests for single or multiple endpoints

---
+++ test/test.middleware.js
@@ -0,0 +1,73 @@
+var restify = require('restify');
+
+var manager = require('../lib');
+
+
+function testMiddleware (req, res, next) {
+  return next();
+}
+
+function testMiddleware2 (req, res, next) {
+  return next();
+}
+
+
+
+exports.setUp = function(done) {
+  done();
+}
+
+exports.tearDown = function(done) {
+  done();
+}
+
+exports.singleMiddleware = function(test) {
+  var endpoints = new manager.EndpointManager();
+  var server = restify.createServer();
+
+  try {
+    endpoints.addEndpoint({
+      name: 'test',
+      description: 'test',
+      method: 'GET',
+      middleware: testMiddleware,
+      fn: function (req, res, next) {
+        return res.send(200);
+      }
+    });
+
+    endpoints.attach(server);
+  } catch (e) {
+    test.equal(e.message, "Path is required in an endpoint definition");
+  }
+
+  test.done();
+}
+
+
+exports.multipleMiddleware = function(test) {
+  var endpoints = new manager.EndpointManager();
+  var server = restify.createServer();
+
+  try {
+    endpoints.addEndpoint({
+      description: 'test',
+      method: 'GET',
+      path: '/test',
+      middleware: [
+        testMiddleware,
+        testMiddleware2
+      ],
+      fn: function (req, res, next) {
+        res.send(200);
+      }
+    });
+
+    endpoints.attach(server);
+  } catch (e) {
+    test.equal(e.message, "Name is required in an endpoint definition");
+  }
+
+  test.done();
+}
+
