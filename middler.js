56933834cf64766e43cb78f4c7fc3af3e0593d07
Erik Kristensen <erik@erikkristensen.com>
2015-03-22 19:58:34 -0600

    middler support

---
+++ middler.js
@@ -0,0 +1,9 @@
+var Endpointer = require('./endpointer.js')
+
+Endpointer.prototype.attachRoute = function(server, endpoint, middleware) {
+  var self = this
+
+  server[endpoint.method.toLowerCase()](endpoint.path, middleware)
+}
+
+module.exports = Endpointer
